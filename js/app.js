029e0aeaedf97a93dbcccb01554cd211c1e99c74
Sharon Clark <clarksl@gmail.com>
2017-01-19 16:19:59 -0700

    rm needed directive; styles for the brush extent

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
     'use strict';
     angular.module("edu.ucar.scied.aq", [
         "edu.ucar.scied.aq.controller",
-        "edu.ucar.scied.chart.directive",
         "edu.ucar.scied.webapp.controller",
         "edu.ucar.scied.webapp.service",
         "edu.ucar.scied.modal.directive",
